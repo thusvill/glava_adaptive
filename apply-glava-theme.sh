aca6eb3339b4a2375a3835239119d1b0ac6964c5
Bios Thusvill <87165098+thusvill@users.noreply.github.com>
2024-06-17 19:16:24 +0530

    Update apply-glava-theme.sh

--- apply-glava-theme.sh
+++ apply-glava-theme.sh
@@ -11,11 +11,11 @@ background_color_2_escaped=$(echo $background_color_2 | sed 's/[\/&]/\\&/g')
 outline_escaped=$(echo $outline | sed 's/[\/&]/\\&/g')
 
 # Replace {color} in C code with background color
-echo 'thusvill@gmail.com' | sudo -S sed -i "s/#define COLOR .*$/#define COLOR (${background_color_escaped} * ((d \/ 90) + 0.5))/" /etc/xdg/glava/radial.glsl
-echo 'thusvill@gmail.com' | sudo -S sed -i "s/#define COLOR .*$/#define COLOR (${background_color_escaped} * ((d \/ 90) + 0.5))/" /etc/xdg/glava/bars.glsl
-echo 'thusvill@gmail.com' | sudo -S sed -i "s/#define COLOR .*$/#define COLOR mix(${background_color_escaped}, ${background_color_2_escaped}, clamp(pos \/ GRADIENT_SCALE, 0, 1))/" /etc/xdg/glava/graph.glsl
+sudo -S sed -i "s/#define COLOR .*$/#define COLOR (${background_color_escaped} * ((d \/ 90) + 0.5))/" /etc/xdg/glava/radial.glsl
+sudo -S sed -i "s/#define COLOR .*$/#define COLOR (${background_color_escaped} * ((d \/ 90) + 0.5))/" /etc/xdg/glava/bars.glsl
+sudo -S sed -i "s/#define COLOR .*$/#define COLOR mix(${background_color_escaped}, ${background_color_2_escaped}, clamp(pos \/ GRADIENT_SCALE, 0, 1))/" /etc/xdg/glava/graph.glsl
 
 # Replace {outline} in the C code
-echo 'thusvill@gmail.com' | sudo -S sed -i "s/#define OUTLINE .*$/#define OUTLINE ${outline_escaped}/" /etc/xdg/glava/circle.glsl
+sudo -S sed -i "s/#define OUTLINE .*$/#define OUTLINE ${outline_escaped}/" /etc/xdg/glava/circle.glsl
 
 exit 0
